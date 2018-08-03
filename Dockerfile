2f6157fac543f573328da6ff278f51d06d94e755
Microclimate User <microclimate.user@localhost>
2018-08-03 03:41:49 +0000

    second commit

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ LABEL maintainer="IBM Java Engineering at IBM Cloud"
 
 COPY target/spring-boot-demo-0.0.1-SNAPSHOT.jar /app.jar
 
-
 ENV JAVA_OPTS=""
 ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar" ]
---
+++ target/custom-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MYSQL_DB_USER ="root"
+MYSQL_DB_PASSWORD="password"
+MYSQL_DB_HOST="article-database"
+MYSQL_DB_PORT="3306"
+
+if [ -z "$MYSQL_DB_USER" ] || [ -z "$MYSQL_DB_PASSWORD" ] || [ -z "$MYSQL_DB_HOST" ] || [ -z "$MYSQL_DB_PORT" ]
+then
+    echo "Environment variables not found. Using default credentials (user michaelbolton, password password, host account-database, port 3306) instead if it has been setup."
+else
+    mysql -u "$MYSQL_DB_USER" --password="$MYSQL_DB_PASSWORD" --host "$MYSQL_DB_HOST" --port "$MYSQL_DB_PORT" < initialize_db.sql
+fi
+
+exec "$@"
